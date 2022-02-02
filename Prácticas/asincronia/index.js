dd37e416837776477c8350e7edc8760048115e2b
Daniel Isai <daisai1228@hotmail.com>
2022-02-01 19:08:52 -0600

    feat:bloque 4, asíncronía

--- Prácticas/asincronia/index.js
+++ Prácticas/asincronia/index.js
@@ -2,6 +2,8 @@
 // console.log('2 Ejecutando')
 // console.log('3 Terminado');
 
+// const { resolve } = require("path/posix")
+
 
 //Ejecucion que toma tiempo siempre va al event queue
 /*
@@ -14,6 +16,7 @@ console.log('3 Terminado');
 
 
 //Solución 1 callbacks (NO ES RECOMENDADA por callbackhell)
+/*
 function run () {
     console.log('1 Inicio')
     myFunction(ejecutando)
@@ -32,3 +35,30 @@ function ejecutando () {
 }
 
 run()
+*/
+
+//Solución 2 promesas
+/*
+Promise: Algo que va a ocurrir pero no sabemos cuando va a ocurrir.
+Estados de las promesas:Pendiente (pending), resuelto, (resolve) y rechazado (reject)
+*/
+
+function asincrona () {
+    return new Promise((response, reject) => {
+        setTimeout(() =>{
+            console.log('2 Ejecutando')
+                response()
+            // reject(new Error ('my custom error'))
+        },1000)
+    }) 
+}
+
+console.log('1 Inicio')
+asincrona().then(() => {
+    console.log('3 Terminado')
+}).catch(err => console.log('error: ', err))
+.finally(() =>{
+    //Se ejecuta siempre, tanto si la promesa se resuelve o no
+    console.log('4 fin'); 
+    console.log('en el finally');
+})
