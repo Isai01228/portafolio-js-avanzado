876b6676b847dd44f68f6222d03c18d52906ca8d
Daniel Isai <daisai1228@hotmail.com>
2022-02-02 20:43:39 -0600

    feat:copia los comentarios

--- Prácticas/servidor3/index.js
+++ Prácticas/servidor3/index.js
@@ -1,16 +1,41 @@
 const http = require('http')
 const url = require('url')
 
+// PETICIONES
+//http://localhost:8080/
+//http://localhost:8080/
+
 const server = http.createServer((req, res) =>{
+
     const urlObject = url.parse(req.url)
     const path = urlObject.path
-    const pathName = urlObject.pathname
-    console.log(`Path: ${path} - pathName: ${pathName}`)
+    // const pathName = urlObject.pathname
+    console.log(`Este es mi path: ${path}`)
 
-    //Validacion de path solicitado
-    //1. OK
-    //2. No tener permisos
+    let status = 0
+    let responseStr = ''
+    let mimeType = {'Content-Type' : 'application/json'}
+
+    if (path === '/') {
+        responseStr = {message: 'OK'}
+        status = 200
+    }else if (path === '/permisos'){
+        responseStr = {message: 'Forbidden'}
+        status = 403
+    }else if (path === '/loquesea.txt') {
+        responseStr = {message: 'Request Time out'}
+        status = 408
+    }else{
+        responseStr = {message: 'Not Found'}
+        status = 404
+    }
+
+    const responseStrinify = JSON.stringify(responseStr)
+
+    res.writeHead(status, mimeType)
+    res.write(responseStrinify)
+    res.end()
+
 })
-
 server.listen(8080)
 console.log('Servidor iniciado')
